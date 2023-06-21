af43a87bd920908b5e185a421bca650284dbb403
João Morais <joao.morais@estsetubal.ips.pt>
2023-06-21 17:31:28 +0100

    First code commit
    This is the first code commit with the base of the project

---
+++ Assets/Scripts/CMFreelookOnlyWhenMiddleMouseDown.cs
@@ -0,0 +1,36 @@
+using Cinemachine;
+using UnityEngine;
+
+public class CMFreelookOnlyWhenMiddleMouseDown : MonoBehaviour
+{
+    void Start()
+    {
+        CinemachineCore.GetInputAxis = GetAxisCustom;
+    }
+    public float GetAxisCustom(string axisName)
+    {
+        if (axisName == "Mouse X")
+        {
+            if (Input.GetMouseButton(2))
+            {
+                return UnityEngine.Input.GetAxis("Mouse X");
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        else if (axisName == "Mouse Y")
+        {
+            if (Input.GetMouseButton(2))
+            {
+                return UnityEngine.Input.GetAxis("Mouse Y");
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        return UnityEngine.Input.GetAxis(axisName);
+    }
+}
